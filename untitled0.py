4c32adb876c0427026508629befa7e10b2e4ad83
KsuBak <56219693+KsuBak@users.noreply.github.com>
2019-11-13 16:16:39 +0200

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 13 16:04:31 2019
+
+@author: student
+"""
+
+ plt.plot( [1, 1+a/8**0.5+1, 1+a/8**0.5+1, a+1], [a+1, a+1+a/8**0.5, a+1+a/8**0.5, a+1])
