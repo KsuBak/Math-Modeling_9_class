da5f7e8b020bc8a6d0d28cfb1f1cf5ac43663370
KsuBak <56219693+KsuBak@users.noreply.github.com>
2019-11-13 16:37:01 +0200

    Add files via upload

---
+++ untitled2.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+def prelomlenie_linsy(b=1):
+    plt.plot([10, 10], [1, 10], marker='^')
+    plt.plot([1, 20], [5, 5])
+    plt.plot([1,10], [8, 8], marker='^')
+    plt.plot([10,20], [8,1], marker='^')
+    plt.show()
+prelomlenie_linsy()
+
+
