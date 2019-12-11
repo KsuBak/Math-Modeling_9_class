e3dd4a296ce4e6ca0158aabb0a3b9b19dc1f9348
KsuBak <56219693+KsuBak@users.noreply.github.com>
2019-12-11 16:06:07 +0200

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import ArtistAnimation
+
+fig = plt.figure()
+
+a = 3
+
+x = np.linspace(-5, 5, 100)
+y = a*x**2
+
+anim_list=[]
+
+for i in range(len(x)):
+    point, = plt.plot(x[i], y[i], 'o')
+    anim_list.append([point])
+
+ani = ArtistAnimation(fig, anim_list, interval=100)
+ani.save('hj.gif')
+    
+    
+     
+
