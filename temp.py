06a86880c944fb2e9b929176daf594e256c7e973
KsuBak <56219693+KsuBak@users.noreply.github.com>
2020-02-12 16:48:14 +0200

    Add files via upload

--- temp.py
+++ temp.py
@@ -5,12 +5,10 @@ from matplotlib.animation import ArtistAnimation
 
 year = 365*24*60*60
 day = 24*60*60
-years = 8
+years = 2
 t = np.arange(0, years*year, 5*day)
 
-G = 6.67*10**(-11) 
-ae = 149.6*10**9
-Msun = 1.98847*10**30
+l = 149 * 10**9
 
 def move_func(s, t):
     (x1, v_x1, y1, v_y1,
@@ -18,63 +16,63 @@ def move_func(s, t):
      x3, v_x3, y3, v_y3)= s
      
     dxdt1= v_x1
-    dv_xdt1 = (-G*m2*(x1-x2)/((x1-x2)**2+(y1-y2)**2)**1.5
-             - G*m3*(x1-x3)/((x1-x3)**2+(y1-y3)**2)**1.5)
+    dv_xdt1 = ( k * q1* q2 / m1 * (x1- x2) / ((x1-x2)**2 + (y1-y2)**2)**1.5
+             + k * q1* q3 / m1 * (x1 - x3) / ((x1 - x3)**2 + (y1 - y3)**2)**1.5)
      
     dydt1= v_y1
-    dv_ydt1=(-G*m2*(y1-x2)/((x1-x2)**2+(y1-y2)**2)**1.5
-            - G*m3*(y1-x3)/((x1-x3)**2+(y1-y3)**2)**1.5)
+    dv_ydt1= ( k * q1* q2 / m1 * (y1- y2) / ((x1-x2)**2 + (y1-y2)**2)**1.5
+            + k * q1* q3 / m1 * (y1 - y3) / ((x1 - x3)**2 + (y1 - y3)**2)**1.5)
             
     dxdt2= v_x2
-    dv_xdt2=(-G*m2*(x2-x1)/((x2-x1)**2+(y2-y1)**2)**1.5
-            - G*m3*(x2-x3)/((x2-x3)**2+(y2-y3)**2)**1.5)
+    dv_xdt2= ( k * q1* q2 / m2 * (x2- x3) / ((x2-x1)**2 + (y2 - y1)**2)**1.5
+            + k * q1* q3 / m2 * (x2- x3) / ((x2 - x3)**2 + (y2 - y3)**2)**1.5)
      
     dydt2= v_y2
-    dv_ydt2=(-G*m2*(y2-y1)/((x2-x1)**2+(y2-y1)**2)**1.5
-            - G*m3*(y2-y3)/((x2-x3)**2+(y2-y3)**2)**1.5)
+    dv_ydt2= ( k * q1* q2 / m2 * (y2- y1) / ((x2-x1)**2 + (y2-y1)**2)**1.5
+            + k * q1* q3 / m2 * (y2 - y3) / ((x2 - x3)**2 + (y2 - y3)**2)**1.5)
      
     dxdt3= v_x3
-    dv_xdt3=(-G*m2*(x3-x1)/((x3-x1)**2+(y3-y1)**2)**1.5
-            - G*m3*(x3-x2)/((x3-x2)**2+(y3-y2)**2)**1.5)
+    dv_xdt3= ( k * q1* q2 / m3 * (x3- x1) / ((x3-x2)**2 + (y3-y1)**2)**1.5
+            + k * q1* q3 / m3 * (x3 - x2) / ((x3 - x2)**2 + (y3 - y2)**2)**1.5)
      
     dydt3= v_y3
-    dv_ydt3=(-G*m2*(y3-y1)/((x3-x1)**2+(y3-y1)**2)**1.5
-            - G*m3*(y3-y2)/((x3-x2)**2+(y3-y2)**2)**1.5)
+    dv_ydt3= ( k * q1* q2 / m3 * (y3- y1) / ((x3-x1)**2 + (y3-y1)**2)**1.5
+            + k * q1* q3 / m3 * (y3 - y2) / ((x3 - x2)**2 + (y3 - y2)**2)**1.5)
      
     return (dxdt1, dv_xdt1, dydt1, dv_ydt1,
              dxdt2, dv_xdt2, dydt2, dv_ydt2, 
              dxdt3, dv_xdt3, dydt3, dv_ydt3 )
 
 
-x11 = 3 * ae
-v_x11 = 0
-y11 = 0
-v_y11 = 15000
+x11 = 0
+v_x11 = -12000
+y11 = (l**2- (l/2)**2)**0.5
+v_y11 = 0
      
-x22=7* ae
+x22= -l/2
 v_x22 = 0
 y22 = 0
-v_y22 = 10000
+v_y22 = -20000
 
-x33=-5* ae
+x33=l/2
 v_x33 = 0
 y33 = 0
-v_y33 = 20000
+v_y33 = 15000
 
 s0 = (x11,v_x11, y11, v_y11,
       x22, v_x22, y22, v_y22,
       x33, v_x33, y33, v_y33)
 
-m1 = 1.5*10**30
-q1 = -1.5*10**20
+m1 = 1*10**30
+q1 = 1*10**20
 
-m2 = 2.5*10**30
-q2 = -2.5*10**20
+m2 = 1*10**30
+q2 = -1*10**20
 
-m3 = 3.5*10**30
-q3 = -3.5*10**20
+m3 = 1*10**30
+q3 = 1*10**20
 
-G = 6.67*10**(-11)
+k = 8.98755 * 10**9
 
 sol = odeint(move_func, s0, t)
 
@@ -91,76 +89,42 @@ for i in range (0, len(t), 1):
     body3, = plt.plot(sol[:i, 8], sol[:i, 10], '-', color='b')
     body3_line, = plt.plot(sol[i, 8], sol[i, 10], 'o', color='b')
     
+                    
+    
     body.append([body1, body1_line, body2, body2_line, body3, body3_line])
     
 ani = ArtistAnimation(fig, body, interval= 100)
 plt.axis('equal')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ani.save('body.gif')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-  
